6f79447dc7737fe27db829f6c4b2912375776508
Caleb Ely <le717@users.noreply.github.com>
2018-12-27 12:38:03 -0500

    Add (ro be trimmed) styles

--- partials/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-  <div>&copy; 2008 &mdash; 2019 <a href="https://www.bricksinmotion.com">Bricks in Motion</a>. All works copyrighted by their respective creators.</div>
-  <div>LEGO is a trademark of the LEGO Group, which does not sponsor, authorize or endorse this web site.</div>
-  <div><a href="https://www.bricksinmotion.com/forums/topic/25304/bricks-in-motion-staff-list/">Site Credits</a> | <a href="https://www.bricksinmotion.com/forums/topic/25303/bricks-in-motion-privacy-policy/">Privacy Policy</a></div>
-</footer>
